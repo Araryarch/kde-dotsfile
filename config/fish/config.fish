ce58c343a698b328299d3051833387dd390c881a
Araryarch <araryaarch@gmail.com>
2026-01-04 12:07:58 +0700

    fix: fix

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,22 @@
+function fish_greeting
+    pokemon-colorscripts -r --no-title
+end
+
 source (/usr/bin/starship init fish --print-full-init | psub)
 
 alias c='clear'
 alias v='nvim'
+alias brd='bun dev'
+alias ga='git add .'
+alias gc='git commit -m'
+alias gp='git push'
+alias brb='bun run build'
+alias prd='pnpm run dev'
+alias prb='pnpm run build'
+alias ls='eza --icons --git'
+alias ll='eza -la --icons --git'
+alias tree='eza --tree --icons'
+alias ngefix='git add . && git commit -m "fix: fix" && git push'
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
